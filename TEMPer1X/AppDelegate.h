5dc83bdc95bbc0fbe7b7bae1a906b21e3fca8aae
Andrew Mannering <andrewm@sledgehammersolutions.co.uk>
2012-03-04 17:24:19 +0000

    Updated copyright statements;

--- TEMPer1X/AppDelegate.h
+++ TEMPer1X/AppDelegate.h
@@ -3,7 +3,7 @@
 //  TEMPer1X
 //
 //  Created by Andrew Mannering on 26/02/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Sledgehammer Solutions Ltd. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
